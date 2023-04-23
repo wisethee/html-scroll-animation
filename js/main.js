70ca7db80082f8e63663044f3ac3f68f0a9c9a2a
Marius Paduraru <31074930+wisethee@users.noreply.github.com>
2023-04-23 12:20:42 +0100

    feat: add functionality

---
+++ js/main.js
@@ -0,0 +1,41 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const startContainer = document.querySelector(".start-container");
+  const endContainer = document.querySelector(".end-container");
+
+  const startContainerOffsetHeight = startContainer.offsetHeight;
+  const endContainerOffsetHeight = endContainer.offsetHeight;
+
+  const startContainerRectTop = startContainer.getBoundingClientRect().top;
+  const endContainerRectTop = endContainer.getBoundingClientRect().top;
+
+  let lastKnownScrollPosition = 0;
+  let ticking = false;
+
+  const animate = (lastKnownScrollPosition) => {
+    const startingPoint = lastKnownScrollPosition - startContainerRectTop;
+    const endPoint = endContainerOffsetHeight - startContainerOffsetHeight;
+
+    if (startingPoint < 0) {
+      startContainer.style.transform = `translateY(0px)`;
+    } else if (startingPoint > 0 && startingPoint <= endPoint) {
+      startContainer.style.transform = `translateY(${startingPoint}px)`;
+      // console.log("starting point: ", startingPoint);
+      // console.log("end point: ", endPoint);
+    } else if (startingPoint > endPoint) {
+      startContainer.style.transform = `translateY(${endPoint}px)`;
+    }
+  };
+
+  document.addEventListener("scroll", () => {
+    lastKnownScrollPosition = window.scrollY;
+
+    if (!ticking) {
+      window.requestAnimationFrame(() => {
+        animate(lastKnownScrollPosition);
+        ticking = false;
+      });
+
+      ticking = true;
+    }
+  });
+});
